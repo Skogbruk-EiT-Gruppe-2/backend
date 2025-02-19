5d7e324e4b8b2df55668bc6b9e3bca91a1fd0dae
Alexander Rathke Johansen <alexarj@stud.ntnu.no>
2025-02-19 09:13:23 +0100

    build: Update api server requirements and db connection

--- api-server/app/db.py
+++ api-server/app/db.py
@@ -1,4 +1,7 @@
+import os
 from pymongo import MongoClient
 
-mongo_client = MongoClient("localhost", 27017)
-db = mongo_client['observationsdb']
+# Use environment variable with a fallback to internal service name
+mongo_uri = os.getenv("MONGO_URI", "mongodb://mongodb:27017")
+client = MongoClient(mongo_uri)
+db = client.get_database("your_database_name")
