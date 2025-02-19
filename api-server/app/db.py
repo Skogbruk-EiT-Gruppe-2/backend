81a4f4d422c7db34676b3b603a1f63282e15fc0f
Alexander Rathke Johansen <alexarj@stud.ntnu.no>
2025-02-19 10:43:52 +0100

    feat: Add auth to api server db connection

--- api-server/app/db.py
+++ api-server/app/db.py
@@ -2,6 +2,9 @@ import os
 from pymongo import MongoClient
 
 # Use environment variable with a fallback to internal service name
-mongo_uri = os.getenv("MONGO_URI", "mongodb://mongodb:27017")
-client = MongoClient(mongo_uri)
-db = client.get_database("your_database_name")
+mongo_uri = os.getenv("MONGO_URI", "mongodb:27017")
+mongo_username = os.getenv("MONGO_USERNAME", "root")
+mongo_password = os.getenv("MONGO_PASSWORD", "root")
+mongo_dbname = os.getenv("MONGO_DBNAME", "observationsdb")
+client = MongoClient(f"mongodb://{mongo_username}:{mongo_password}@{mongo_uri}")
+db = client.get_database(mongo_dbname)
